ee88e603f4d6bdddd56afad015b7fed710a2acf3
wupojung <wupojung@gmail.com>
2023-07-04 02:30:47 +0800

    add led test

--- RPI-LED/Program.cs
+++ RPI-LED/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Device.Gpio;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello");
+
+        GpioController controller = new GpioController(PinNumberingScheme.Board);
+
+        controller.OpenPin(10,PinMode.Output);
+
+        while (true)
+        {
+            controller.Write(10,PinValue.High);
+            Thread.Sleep(200);
+            controller.Write(10,PinValue.Low);
+            Thread.Sleep(200);
+        }
+        
+        controller.ClosePin(10);
+    }
+}
